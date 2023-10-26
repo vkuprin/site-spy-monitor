c31276703db18c89469375407677211f506d9c75
Vitaly Kuprin <vitas_s@inbox.lv>
2023-10-26 02:36:56 +0200

    style: improve style deisgn UX

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -6,8 +6,8 @@ import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import { CloseOutlined } from '@ant-design/icons';
 
 import './styles.scss';
-import useStorage from "@src/shared/hooks/useStorage";
-import trackedWebsitesStorage, {websitesStorage} from "@src/shared/storages/trackedWebsitesStorage";
+
+import trackedWebsitesStorage from '@src/shared/storages/trackedWebsitesStorage';
 
 const Popup = (): ReactElement => {
   // const storageData = useStorage(websitesStorage);
@@ -30,13 +30,13 @@ const Popup = (): ReactElement => {
     if (url && intervalTime) {
       const newWebsite = {
         url: url,
-        content: '',  // initially empty, can be populated later
+        content: '', // initially empty, can be populated later
       };
       await trackedWebsitesStorage.addWebsite(newWebsite);
       setTrackedWebsites(prev => [...prev, url]);
       setUrl('');
     }
-  }
+  };
 
   const checkWebsiteChanges = async (url: string): Promise<void> => {
     const isSameVersion = await trackedWebsitesStorage.isVersionSame(url);
@@ -52,83 +52,87 @@ const Popup = (): ReactElement => {
       const currentCount = prev[url] || 0;
       return { ...prev, [url]: currentCount + 1 };
     });
-  }
+  };
 
   const handleRemoveWebsite = async (websiteUrl: string) => {
     await trackedWebsitesStorage.removeWebsite(websiteUrl);
     setTrackedWebsites(prev => prev.filter(w => w !== websiteUrl));
-  }
+  };
 
   useEffect(() => {
-    const intervals = trackedWebsites.map((website) => {
+    const intervals = trackedWebsites.map(website => {
       return setInterval(async (): Promise<void> => {
         await checkWebsiteChanges(website);
       }, intervalTime * 1000);
     });
 
     return () => {
-      intervals.forEach((interval) => {
+      intervals.forEach(interval => {
         clearInterval(interval);
       });
     };
   }, [trackedWebsites, intervalTime, hasChanged]);
 
   return (
-      <div className="container">
-        <h1>Website Tracker</h1>
-        <p>Enter the URL of a website to track:</p>
+    <div className="container">
+      <div className="container-selector">
+        <h1>Enter the URL of a website to track:</h1>
         <div className="btn-container">
-          <Input value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Website URL" />
-          <Button className="btn-track" onClick={handleStartTracking}>
-            Track
-          </Button>
+          <Input value={url} onChange={e => setUrl(e.target.value)} placeholder="Website URL" />
         </div>
-        {url && (
-            <>
-              <p>Enter an interval to check for changes:</p>
-              <Select
-                  style={{ width: '100%' }}
-                  placeholder="Interval"
-                  onChange={(value) => setIntervalTime(value)}
-                  value={intervalTime}
-              >
-                <Select.Option value={15}>15s</Select.Option>
-                <Select.Option value={30}>30s</Select.Option>
-                <Select.Option value={60}>1m</Select.Option>
-              </Select>
-            </>
-        )}
-        <List
-            itemLayout="horizontal"
-            dataSource={trackedWebsites}
-            renderItem={(website) => (
-                <List.Item
-                    actions={[
-                      <CloseOutlined key="list-close" onClick={async () => { await handleRemoveWebsite(website) }} />
-                    ]}
-                    style={{ backgroundColor: hasChanged[website] ? 'red' : 'green' }}
-                >
-                  <List.Item.Meta
-                      avatar={
-                        <img
-                            style={{
-                              width: '16px',
-                              height: '16px',
-                              borderRadius: '50%',
-                              marginRight: '10px'
-                            }}
-                            src={`${website}/favicon.ico`}
-                            alt=""
-                        />
-                      }
-                      title={website}
-                      description={`Counter: ${checkCount[website] || 0}, Next check in: ${intervalTime - ((checkCount[website] || 0) % intervalTime)}s`}
-                  />
-                </List.Item>
-            )}
-        />
       </div>
+      <div className="container-selector">
+        <h1>Enter an interval to check for changes:</h1>
+        <Select
+          style={{ width: '100%' }}
+          placeholder="Interval"
+          onChange={value => setIntervalTime(value)}
+          value={intervalTime}>
+          <Select.Option value={15}>15s</Select.Option>
+          <Select.Option value={30}>30s</Select.Option>
+          <Select.Option value={60}>1m</Select.Option>
+        </Select>
+      </div>
+      <Button className="btn-track" onClick={handleStartTracking} style={{ color: '#fff' }}>
+        Track
+      </Button>
+      <List
+        itemLayout="horizontal"
+        dataSource={trackedWebsites}
+        renderItem={website => (
+          <List.Item
+            actions={[
+              <CloseOutlined
+                key="list-close"
+                onClick={async () => {
+                  await handleRemoveWebsite(website);
+                }}
+              />,
+            ]}
+            style={{ border: '1px solid white', borderColor: hasChanged[website] ? 'red' : 'green' }}>
+            <List.Item.Meta
+              avatar={
+                <img
+                  style={{
+                    width: '16px',
+                    height: '16px',
+                    borderRadius: '50%',
+                    marginRight: '10px',
+                  }}
+                  src={`${website}/favicon.ico`}
+                  alt=""
+                />
+              }
+              title={website}
+              description={`Counter: ${checkCount[website] || 0}, Next check in: ${
+                intervalTime - ((checkCount[website] || 0) % intervalTime)
+              }s`}
+            />
+          </List.Item>
+        )}
+      />
+    </div>
   );
-}
+};
 
 export default withErrorBoundary(withSuspense(Popup, <div> Loading ... </div>), <div> Error Occur </div>);
