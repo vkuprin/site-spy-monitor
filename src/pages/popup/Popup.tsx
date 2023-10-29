f992d66e4faf421cefdfdfbac9936b558fa6681a
Vitaly Kuprin <vitas_s@inbox.lv>
2023-10-29 13:48:46 +0100

    style: improve styling

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, ReactElement } from 'react';
-import { Button, Input, List, notification, Select } from 'antd';
+import { Button, Input, List, notification, Select, ConfigProvider, theme } from 'antd';
 import '@pages/popup/Popup.css';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
-import { CloseOutlined } from '@ant-design/icons';
+import { CloseOutlined, GlobalOutlined } from '@ant-design/icons';
 
 import './styles.scss';
 
@@ -13,7 +13,7 @@ const Popup = (): ReactElement => {
   // const storageData = useStorage(websitesStorage);
 
   const [url, setUrl] = useState('');
-  const [intervalTime, setIntervalTime] = useState<number>(15); // default value set to 15 seconds
+  const [intervalTime, setIntervalTime] = useState<number>(15);
   const [trackedWebsites, setTrackedWebsites] = useState<string[]>([]);
   const [checkCount, setCheckCount] = useState<Record<string, number>>({});
   const [hasChanged, setHasChanged] = useState<Record<string, boolean>>({});
@@ -74,64 +74,69 @@ const Popup = (): ReactElement => {
   }, [trackedWebsites, intervalTime, hasChanged]);
 
   return (
-    <div className="container">
-      <div className="container-selector">
-        <h1>Enter the URL of a website to track:</h1>
-        <div className="btn-container">
-          <Input value={url} onChange={e => setUrl(e.target.value)} placeholder="Website URL" />
+    <ConfigProvider
+      theme={{
+        algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
+      }}>
+      <div className="container">
+        <header className="header">
+          <h1
+            style={{
+              marginLeft: 10,
+            }}>
+            Vee Tracker
+          </h1>
+        </header>
+        <div className="container-selector">
+          <h1 className="container-selector__title">Enter the URL of a website to track:</h1>
+          <div className="btn-container">
+            <Input value={url} onChange={e => setUrl(e.target.value)} placeholder="Website URL" />
+          </div>
         </div>
-      </div>
-      <div className="container-selector">
-        <h1>Enter an interval to check for changes:</h1>
-        <Select
-          style={{ width: '100%' }}
-          placeholder="Interval"
-          onChange={value => setIntervalTime(value)}
-          value={intervalTime}>
-          <Select.Option value={15}>15s</Select.Option>
-          <Select.Option value={30}>30s</Select.Option>
-          <Select.Option value={60}>1m</Select.Option>
-        </Select>
-      </div>
-      <Button className="btn-track" onClick={handleStartTracking} style={{ color: '#fff' }}>
-        Track
-      </Button>
-      <List
-        itemLayout="horizontal"
-        dataSource={trackedWebsites}
-        renderItem={website => (
-          <List.Item
-            actions={[
-              <CloseOutlined
-                key="list-close"
-                onClick={async () => {
-                  await handleRemoveWebsite(website);
-                }}
-              />,
-            ]}
-            style={{ border: '1px solid white', borderColor: hasChanged[website] ? 'red' : 'green' }}>
-            <List.Item.Meta
-              avatar={
-                <img
-                  style={{
-                    width: '16px',
-                    height: '16px',
-                    borderRadius: '50%',
-                    marginRight: '10px',
+        <div className="container-selector">
+          <h1 className="container-selector__title">Enter an interval to check for changes:</h1>
+          <Select
+            className="input--full-width"
+            placeholder="Interval"
+            onChange={value => setIntervalTime(value)}
+            value={intervalTime}>
+            <Select.Option value={15}>15s</Select.Option>
+            <Select.Option value={30}>30s</Select.Option>
+            <Select.Option value={60}>1m</Select.Option>
+          </Select>
+        </div>
+        <Button className="btn--track text--white" onClick={handleStartTracking}>
+          Track
+        </Button>
+        <List
+          itemLayout="horizontal"
+          dataSource={trackedWebsites}
+          renderItem={website => (
+            <List.Item
+              actions={[
+                <CloseOutlined
+                  key="list-close"
+                  onClick={async () => {
+                    await handleRemoveWebsite(website);
                   }}
-                  src={`${website}/favicon.ico`}
-                  alt=""
-                />
-              }
-              title={website}
-              description={`Counter: ${checkCount[website] || 0}, Next check in: ${
-                intervalTime - ((checkCount[website] || 0) % intervalTime)
-              }s`}
-            />
-          </List.Item>
-        )}
-      />
-    </div>
+                />,
+              ]}
+              className={`border--dynamic ${hasChanged[website] ? 'border--red' : 'border--green'}`}>
+              <List.Item.Meta
+                avatar={<img className="favicon" src={`${website}/favicon.ico`} alt="" />}
+                title={<span className="list__meta-title">{website}</span>}
+                description={
+                  <span className="list__meta-description">
+                    Counter: {checkCount[website] || 0}, Next check in:
+                    {intervalTime - ((checkCount[website] || 0) % intervalTime)}s
+                  </span>
+                }
+              />
+            </List.Item>
+          )}
+        />
+      </div>
+    </ConfigProvider>
   );
 };
 
