9a60e6c486c94be97b60a1fb44a026fa31bcca23
Vitaly Kuprin <vitas_s@inbox.lv>
2023-11-08 21:27:22 +0100

    feat: add no-cors options

--- utils/helpers/fetchWebsiteSize.ts
+++ utils/helpers/fetchWebsiteSize.ts
@@ -1,6 +1,8 @@
 const fetchWebsiteSize = async (url: string): Promise<number> => {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      mode: 'no-cors',
+    });
     const content = await response.text();
     return new Blob([content]).size;
   } catch (error) {
--- utils/helpers/getWebsiteContent.ts
+++ utils/helpers/getWebsiteContent.ts
@@ -1,27 +1,29 @@
 const getWebsiteContent = async (url: string): Promise<string> => {
   try {
-    const response = await fetch(url)
-    const htmlContent = await response.text()
+    const response = await fetch(url, {
+      mode: 'no-cors',
+    });
+    const htmlContent = await response.text();
 
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(htmlContent, 'text/html')
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlContent, 'text/html');
 
     // Get the body content
-    const body = doc.body
+    const body = doc.body;
 
     // Remove all script tags
-    const scripts = Array.from(body.getElementsByTagName('script'))
+    const scripts = Array.from(body.getElementsByTagName('script'));
     for (const script of scripts) {
-      script.parentNode.removeChild(script)
+      script.parentNode.removeChild(script);
     }
 
     // Remove all html tags and return the plain text
-    const withoutScripts = body.innerHTML.replace(/<[^>]*>?/gm, '')
-    return withoutScripts
+    const withoutScripts = body.innerHTML.replace(/<[^>]*>?/gm, '');
+    return withoutScripts;
   } catch (error) {
-    console.error('Error fetching website content:', error)
-    return ''
+    console.error('Error fetching website content:', error);
+    return '';
   }
-}
+};
 
-export default getWebsiteContent
+export default getWebsiteContent;
