b2ebcff0eb566e58dfd1e426970f407593abe034
Vitaly Kuprin <vitas_s@inbox.lv>
2023-11-08 22:43:29 +0100

    fix: correct cors fix

--- manifest.ts
+++ manifest.ts
@@ -11,30 +11,31 @@ const manifest: chrome.runtime.ManifestV3 = {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   permissions: ['alarms', 'notifications', 'storage'],
-  options_page: 'src/pages/options/index.html',
+  // options_page: 'src/pages/options/index.html',
   background: {
     service_worker: 'src/pages/background/index.js',
     type: 'module',
   },
+  host_permissions: ['http://*/*', 'https://*/*'],
   action: {
     default_popup: 'src/pages/popup/index.html',
     default_icon: 'icon-34.png',
   },
-  chrome_url_overrides: {
-    newtab: 'src/pages/newtab/index.html',
-  },
+  // chrome_url_overrides: {
+  //   newtab: 'src/pages/newtab/index.html',
+  // },
   icons: {
     '128': 'icon-128.png',
   },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['src/pages/content/index.js'],
-      // KEY for cache invalidation
-      css: ['assets/css/contentStyle<KEY>.chunk.css'],
-    },
-  ],
-  devtools_page: 'src/pages/devtools/index.html',
+  // content_scripts: [
+  //   {
+  //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+  //     js: ['src/pages/content/index.js'],
+  //     // KEY for cache invalidation
+  //     css: ['assets/css/contentStyle<KEY>.chunk.css'],
+  //   },
+  // ],
+  // devtools_page: 'src/pages/devtools/index.html',
   web_accessible_resources: [
     {
       resources: ['assets/js/*.js', 'assets/css/*.css', 'icon-128.png', 'icon-34.png'],
